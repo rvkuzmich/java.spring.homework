349409eb6e365e3733fc2c568cf9cfb982afa82d
Roman Kuzmich <124430758+rvkuzmich@users.noreply.github.com>
2024-10-16 10:09:45 +0300

    Create Role.java

---
+++ hw7/src/main/java/kuzmich/hw7/model/Role.java
@@ -0,0 +1,27 @@
+package kuzmich.hw7.model;
+
+//import jakarta.persistance.*;
+
+//@Entity
+//@Table(name = "roles")
+public enum Role {
+
+  ADMIN("admin"), USER("user");
+
+  private final String name;
+
+  Role(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+//  @Id
+//  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+//  private Long id;
+
+//  @Column(name = "name")
+//  private String name;
+  
+}
