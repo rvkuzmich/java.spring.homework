a77ce4c62e8aa2297f588ac1b51f8229a477209d
Roman Kuzmich <124430758+rvkuzmich@users.noreply.github.com>
2024-10-16 09:11:01 +0300

    Update MyCustomUserDetailsService.java

--- hw7/src/main/java/kuzmich/hw7/security/MyCustomUserDetailsService.java
+++ hw7/src/main/java/kuzmich/hw7/security/MyCustomUserDetailsService.java
@@ -6,8 +6,13 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class MyCustomUserDetailsService implements UserDetailsService {
 
+    private final UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Optional<User> user = userRepository.findByLogin(username)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        
+        return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getPassword(), List.of());
     }
 }
