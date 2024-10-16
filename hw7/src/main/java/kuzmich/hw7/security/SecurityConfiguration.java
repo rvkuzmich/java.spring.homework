0c0bd9a925992b5eddec2d1acdd56b13268e7922
Roman Kuzmich <124430758+rvkuzmich@users.noreply.github.com>
2024-10-16 09:54:19 +0300

    Update SecurityConfiguration.java

--- hw7/src/main/java/kuzmich/hw7/security/SecurityConfiguration.java
+++ hw7/src/main/java/kuzmich/hw7/security/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,5 +19,10 @@ public class SecurityConfiguration {
                 .formLogin(Customizer.withDefaults())
 //                .formLogin(it -> it.loginPage("/my-login.html"))
                 .build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
