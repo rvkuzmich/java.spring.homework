953a35931e8ec7ff372c09cb1119e35bc14f85b3
Roman Kuzmich <124430758+rvkuzmich@users.noreply.github.com>
2024-10-17 09:59:30 +0300

    Create ProjectPageController.java

---
+++ hw8/src/main/java/kuzmich/hw8/controllers/ProjectPageController.java
@@ -0,0 +1,37 @@
+package kuzmich.hw8.controllers;
+
+import kuzmich.hw8.services.ProjectPageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/home/projects")
+@RequiredArgsConstructor
+public class ProjectPageController {
+
+    private final ProjectPageService projectPageService;
+
+    @GetMapping
+    public String getAllProjectsPage (Model model) {
+        List<ProjectPageDto> projects = projectPageService.findAllProjects();
+        model.addAttribute("projects", projects);
+        return "projects-page.html";
+    }
+
+    @GetMapping("/{id}")
+    public String getProjectPage(@PathVariable Long id, Model model) {
+        Optional<ProjectPageDto> projectOptional = projectPageService.findProjectById(id);
+        if(projectOptional.isEmpty()) {
+            return "not-found.html";
+        }
+        model.addAttribute("project", projectOptional.get());
+        return "project-page.html";
+    }
+}
