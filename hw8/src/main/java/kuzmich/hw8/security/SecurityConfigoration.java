757fca069e5edf5b512478770302d42bc0153e46
Roman Kuzmich <124430758+rvkuzmich@users.noreply.github.com>
2024-10-17 10:14:10 +0300

    Create SecurityConfigoration.java

---
+++ hw8/src/main/java/kuzmich/hw8/security/SecurityConfigoration.java
@@ -0,0 +1,39 @@
+package kuzmich.hw8.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        return new GrantedAuthorityDefaults("MY_ROLE_PREFIX_");
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/home/projects/**").hasAuthority("admin")
+//                        .requestMatchers.("/home/projects/**").hasRole("admin") //MY_ROLE_PREFIX_admin
+                        .requestMatchers("/home/timesheets/**").hasAnyAuthority("admin", "user")
+                        .anyRequest().hasAuthority("rest")
+                )
+                .formLogin(Customizer.withDefaults())
+//                .formLogin(it -> it.loginPage("/my-login.html"))
+                .csrf().disable()
+                .build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
