6f8db45c441bf76da8dd4f2acd36732955f11552
Roman Kuzmich <124430758+rvkuzmich@users.noreply.github.com>
2024-10-17 10:05:05 +0300

    Create User.java

---
+++ hw8/src/main/java/kuzmich/hw8/model/User.java
@@ -0,0 +1,32 @@
+package kuzmich.hw8.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "users")
+@Schema(description = "Тип пользователя")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long userId;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "userId"),
+            inverseJoinColumns = @JoinColumn(name = "role_id",
+                    referencedColumnName = "roleId"))
+    @Schema(description = "Роли пользователя")
+    private Set<Role> roles;
+}
