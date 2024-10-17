4fd55b06e998e5128316b01ee549e2901a265c34
Roman Kuzmich <124430758+rvkuzmich@users.noreply.github.com>
2024-10-17 09:58:23 +0300

    Create ExceptionController.java

---
+++ hw8/src/main/java/kuzmich/hw8/controllers/ExceptionController.java
@@ -0,0 +1,26 @@
+package kuzmich.hw8.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<?> handleNoSuchElementException(NoSuchElementException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+//    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> exceptionHandler(Exception e) {
+        return ResponseEntity.internalServerError().body("Something's go wrong");
+    }
+}
