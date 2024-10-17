99461a4e2c3f5efb3d118780bf052c43ec3a0145
Roman Kuzmich <124430758+rvkuzmich@users.noreply.github.com>
2024-10-17 11:19:21 +0300

    Update LoggingAspect.java

--- hw8/src/main/java/kuzmich/hw8/aspect/LoggingAspect.java
+++ hw8/src/main/java/kuzmich/hw8/aspect/LoggingAspect.java
@@ -1,1 +1,21 @@
+package kuzmich.hw8.aspect;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+  @Before(value = "execution(* kuzmich.hw8.services.TimesheetService.*(..))")
+  public void beforeTimesheetServiceFindById(JoinPoint jp) {
+    String methodName = jp.getSignature().getName();
+//    Long id = (Long) jp.getArgs()[0];
+    log.info("Before -> TimesheetService#{}", methodName);
+  }
+  
+}
