8f47df1f6bcf3caca919c0e50963672ff8a540d6
Roman Kuzmich <124430758+rvkuzmich@users.noreply.github.com>
2024-10-17 10:09:05 +0300

    Update ProjectRepository.java

--- hw8/src/main/java/kuzmich/hw8/repositories/ProjectRepository.java
+++ hw8/src/main/java/kuzmich/hw8/repositories/ProjectRepository.java
@@ -1,6 +1,6 @@
 package kuzmich.hw8.repositories;
 
-import kuzmich.hw7.model.Project;
+import kuzmich.hw8.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
